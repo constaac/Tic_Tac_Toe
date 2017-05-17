64a4e69c696ed05b7ef1590527be4793341cbadd
constaac <aconstantine719@gmail.com>
2017-05-16 21:22:39 -0400

    Starting work on Displaying game history, create game functions, error messages have been added under each button on the left panel

--- assets/scripts/game/ui.js
+++ assets/scripts/game/ui.js
@@ -2,10 +2,23 @@
 
 const createGameSuccess = function (response) {
   console.log(response)
+  $('.gameid-indicator').text(response.game.id)
+  $('.create-error').css('color', 'green')
+  $('.create-error').text('Game Created!')
+  $('.create-error').css('visibility', 'visible')
+  setTimeout(function () {
+    $('.create-error').css('visibility', 'hidden')
+  }, 2000)
 }
 
 const createGameFailure = function (response) {
   console.log(response)
+  $('.create-error').css('color', 'red')
+  $('.create-error').text('Error creating game!')
+  $('.create-error').css('visibility', 'visible')
+  setTimeout(function () {
+    $('.create-error').css('visibility', 'hidden')
+  }, 2000)
 }
 
 module.exports = {
